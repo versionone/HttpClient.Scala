313115200e4776010f771a1b63a229642a01ba58
Joe Koberg <joe.koberg@versionone.com>
2014-04-18 17:01:16 -0400

    Move sample app and extract the JSON parsing of OAuth2 settings

--- src/test/scala/com/versionone/httpclient/sampleApp/SampleApp.scala
+++ src/test/scala/com/versionone/httpclient/sampleApp/SampleApp.scala
@@ -1,60 +1,20 @@
 package com.versionone.httpclient.sampleApp
 
 import com.versionone.httpclient._
-import org.apache.oltu.oauth2.common.token._
-
-import scala.util.parsing.json.JSON
-import scala.io.Source
-
 import org.slf4j.LoggerFactory
 
-
 object SampleApp {
-  def parseFile (filename:String) = JSON parseFull Source.fromFile(filename).mkString
-  
-  def readCreds(storedCreds:String) = {
-    val Some(JMap(creds)) = parseFile(storedCreds)
-    val JStr(scope) = creds("scope")
-    val JStr(refreshToken) = creds("refresh_token")
-    val JStr(accessToken) = creds("access_token")
-    new BasicOAuthToken(
-	      accessToken,
-	      600, // TODO see if this is used anywhere and get rid of magic number
-	      refreshToken,
-	      scope
-	      )
-  }
-  
-  def readSettings(clientSecrets:String, storedCreds:String) = {
-    val Some(JMap(secrets)) = parseFile(clientSecrets)
-    val JMap(installed) = secrets("installed")
-    val JStr(clientId) = installed("client_id")
-    val JStr(clientSecret) = installed("client_secret")
-    val JStr(authUri) = installed("auth_uri")
-    val JStr(tokenUri) = installed("token_uri")
-    val JStr(serverBase) = installed("server_base_uri")
-    val JList(redirectUris) = installed("redirect_uris")
-    val redirs = for (JStr(url) <- redirectUris) yield url
-    OAuth2Settings(
-	  readCreds(storedCreds),
-	  clientId,
-	  clientSecret, 
-	  serverBase,
-	  tokenUri,
-	  authUri,
-	  redirs.head
-	  )
-  }
-  
   def main(args: Array[String]): Unit = {
-    val settings = readSettings("client_secrets.json", "stored_credentials.json")
+    val settings = OAuth2Settings.fromFiles(
+        "client_secrets.json",
+        "stored_credentials.json" )
     val l4jlog = LoggerFactory.getLogger("SampleApp")
-    val log = new SimpleLogger {
+    val logAdapter = new SimpleLogger {
       def debug(s:String) = l4jlog debug s
       def info(s:String) = l4jlog info s
       def error(s:String) = l4jlog error s
     }
-    val client = new V1HttpClient(settings, log, "ClarityOne-Test/1.0")
+    val client = new V1HttpClient(settings, logAdapter, "V1Http-Sample/1.0")
     val me = client.Query("""
       from: Member
       select:
