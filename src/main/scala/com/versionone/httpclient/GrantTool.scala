cc01518551ea15e0e6501d96e9fa264aeb9c28b2
Joe Koberg <joe.koberg@versionone.com>
2014-05-13 12:14:06 -0400

    WIP grant tool facility to do the permission grant on behalf of the user

--- src/main/scala/com/versionone/httpclient/GrantTool.scala
+++ src/main/scala/com/versionone/httpclient/GrantTool.scala
@@ -5,23 +5,111 @@ import org.apache.oltu.oauth2.common.message.types._
 import scala.language.postfixOps
 import scala.util.parsing.json._
 import sys.process._
+import java.net._
+import com.versionone.httpclient._
+
+import scala.io.Source
 
 /**
  * @author ${user.name}
  */
+
+object V1Oauth2Api {
+  
+  def makeSecrets(baseurl:String, username:String, password:String) : Option[Map[String,Any]] = { 
+    val url = new URL(baseurl + "/ClientRegistration.mvc/register")
+    url.openConnection() match {
+      case conn : HttpURLConnection =>
+        val auth = new sun.misc.BASE64Encoder().encode((username + ":" + password).getBytes())
+        conn.setRequestProperty("Authorization", "Basic " + auth); 
+        conn.setDoOutput(true)
+        conn.setRequestMethod("POST")
+        val datestr = new java.util.Date().toGMTString()
+        val writer = new java.io.OutputStreamWriter(conn.getOutputStream())
+        writer.write(s"""{
+          "client_name": "Integration ${new java.util.Date().toGMTString()}",
+          "client_type": "Public",
+          "client_apikey": "",
+          "redirect_uri": "urn:ietf:wg:oauth:2.0:oob"
+          }""")
+        writer.close()
+        if (conn.getResponseCode() != HttpURLConnection.HTTP_OK)
+          sys error s"Unable to register client. ${conn.getResponseMessage()}"
+        else {
+          val body = JSON.parseFull(Source.fromInputStream(conn.getInputStream()).mkString)
+          for {
+            Some(JMap(response)) <- body
+            JStr(client_id) = response("client_id")
+            JStr(client_name) = response("client_name")
+            JStr(redirect_uri) = response("redirect_uri")
+            JStr(client_secret) = response("client_secret")
+            JStr(server_base_uri) = response("server_base_uri")
+            JStr(auth_uri) = response("auth_uri")
+            JStr(token_uri) = response("token_uri")
+          } yield
+            Map("installed" -> response)
+        }
+    }
+  }
+  
+  def getGrantCode(baseUrl:String, grantUrl:String, username:String, password:String) : String = { 
+    val url = new URL(grantUrl)
+    url.openConnection() match {
+      case conn : HttpURLConnection =>
+        val auth = new sun.misc.BASE64Encoder().encode((username + ":" + password).getBytes())
+        conn.setRequestProperty("Authorization", "Basic " + auth); 
+        conn.setRequestMethod("GET")
+        if (conn.getResponseCode() != HttpURLConnection.HTTP_OK)
+          sys error s"Unable to get grant page. ${conn.getResponseMessage()}"
+        else {
+          val body = Source.fromInputStream(conn.getInputStream()).mkString
+          val x = xml.XML loadString body
+          val action = x \\ "form" \ "@action"
+          val auth_request = x \\ "input" \ "@value"
+          val postUrl = new URL(baseUrl + action)
+          postUrl.openConnection match {
+            case conn : HttpURLConnection =>
+              conn.setRequestProperty("Authorization", "Basic " + auth); 
+              conn.setDoOutput(true)
+              conn.setRequestMethod("POST")
+              val writer = new java.io.OutputStreamWriter(conn.getOutputStream())
+              writer.write(s"""allow=true&auth_request=$auth_request""")
+              writer.close()
+              if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                sys error "freakout"
+              } else {
+                val body = Source.fromInputStream(conn.getInputStream()).mkString
+                val x = xml.XML loadString body
+                val code = (x \\ "textarea").text
+                code
+              }
+          }
+        }
+    }
+  }
+  
+  
+}
+
 object GrantTool extends App {
   case class Args(
-    secrets: String       = "client_secrets.json",
-    creds:   String       = "stored_credentials.json",
-    scopes:  List[String] = Nil
+    secrets:  String         = "client_secrets.json",
+    creds:    String         = "stored_credentials.json",
+    scopes:   List[String]   = Nil,
+    username: Option[String] = None,
+    password: Option[String] = None,
+    url:      Option[String] = None
     )
     
   def parseArgs (parsed:Args, remainingArgs:List[String]) : Args  =
     remainingArgs match {
       case Nil => parsed
-      case "--secrets" :: secrets :: tail => parseArgs(parsed.copy(secrets = secrets), tail)
-      case "--creds"   :: creds   :: tail => parseArgs(parsed.copy(creds = creds), tail)    
-      case                scope   :: tail => parseArgs(parsed.copy(scopes = scope::parsed.scopes), tail)
+      case "--secrets"  :: secrets :: tail => parseArgs(parsed.copy(secrets  = secrets),              tail)
+      case "--creds"    :: creds   :: tail => parseArgs(parsed.copy(creds    = creds),                tail)
+      case "--username" :: user    :: tail => parseArgs(parsed.copy(username = Some(user)),           tail)
+      case "--password" :: pass    :: tail => parseArgs(parsed.copy(password = Some(pass)),           tail)
+      case "--url"      :: url     :: tail => parseArgs(parsed.copy(url      = Some(url)),            tail)
+      case                 scope   :: tail => parseArgs(parsed.copy(scopes   = scope::parsed.scopes), tail)
     }
   
   val myargs = parseArgs(new Args(), args toList)
@@ -46,7 +134,7 @@ object GrantTool extends App {
 
     //   await pasted auth code
     val code = System.console().readLine()
-    println("Code received. Attemping to trade for access token...")
+    println("\nCode received. Attemping to trade for access token...")
     
     //   contact server token endpoint
     val req = request.OAuthClientRequest
@@ -69,6 +157,6 @@ object GrantTool extends App {
       "expires_in"    -> creds.getExpiresIn()
     )).toString())
     p.close()
-    println(s"\ncredentials written to ${myargs.creds}")
+    println(s"\nSuccess. Credentials written to ${myargs.creds}")
   }
 }
